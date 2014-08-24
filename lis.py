40bbed8abaccd14edd006cc4cdf887600f54c814
Chris Fuller <chris@cjwfuller.com>
2014-08-24 10:09:27 +0100

    refactor LIS back out of class, simplify tests

--- lis.py
+++ lis.py
@@ -5,16 +5,14 @@
 # O(n^2) complexity
 #
 
-class Lis:
+def lis(sequence):
+    all_lis = [[sequence[0]]]
+    for idx, s in enumerate(sequence):
+        endx = sequence[idx]
+        for idy, l in enumerate(all_lis):
+            endy = all_lis[idy][-1]
+            if(s > endy):
+                all_lis.append(all_lis[idy] + [s])
 
-    def lis(self, sequence):
-        all_lis = [[sequence[0]]]
-        for idx, s in enumerate(sequence):
-            endx = sequence[idx]
-            for idy, l in enumerate(all_lis):
-                endy = all_lis[idy][-1]
-                if(s > endy):
-                    all_lis.append(all_lis[idy] + [s])
-
-        max_len = max(map(len, all_lis))
-        return [m for m in all_lis if len(m) == max_len]
+    max_len = max(map(len, all_lis))
+    return [m for m in all_lis if len(m) == max_len]
--- test_lis.py
+++ test_lis.py
@@ -3,9 +3,14 @@ import lis
 
 class TestLis(unittest.TestCase):
     def test_basic(self):
-        l = lis.Lis()
-        answer = [[0, 4, 6, 9, 13, 15], [0, 2, 6, 9, 13, 15], [0, 4, 6, 9, 11, 15], [0, 2, 6, 9, 11, 15]]
-        self.assertEquals(answer, l.lis([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
+        answer = [
+            [0, 4, 6, 9, 13, 15],
+            [0, 2, 6, 9, 13, 15],
+            [0, 4, 6, 9, 11, 15],
+            [0, 2, 6, 9, 11, 15]
+        ]
+
+        self.assertEquals(answer, lis.lis([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
 
 if __name__ == '__main__':
     unittest.main()
